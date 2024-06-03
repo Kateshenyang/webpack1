a7c177f0014f0b849996511946bb98d4ca433ff0
Ekaterina <kateshenyang@gmail.com>
2024-06-03 22:02:46 +0300

    import export done

--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -1,6 +1,10 @@
-// TODO: write your code here
+const game = new Game();
+game.start();
+
 import sum from './basic';
 
 console.log('app worked');
 
 console.log(sum([1, 2]));
+
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
